d1b81dc592a44ed14b4003b0a741b0699e81f220
Jeff Wahome <jeff.rureri@student.cuk.ac.ke>
2023-01-11 22:42:20 +0300

    csrf change

--- Church_website/settings.py
+++ Church_website/settings.py
@@ -31,6 +31,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['pceakitengelayouth.up.railway.app']
 
+CSRF_TRUSTED_ORIGINS=['pceakitengelayouth.up.railway.app']
+
 # Application definition
 
 INSTALLED_APPS = [
